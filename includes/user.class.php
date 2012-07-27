a72b1eb663a7314416dd8695997305fc6766bf2b
Luke Stevenson <luke@lucanos.com>
2012-07-27 22:35:23 +1000

    Replaced TABs with DOUBLE-SPACE
    Personal preference

--- includes/user.class.php
+++ includes/user.class.php
@@ -3,39 +3,39 @@
 include( 'config.php' );
 
 class User {
-	
-	function __construct(){}
+  
+  function __construct(){}
 
-	function randomString( $len=32 ){
+  function randomString( $len=32 ){
     // Initialise a string
-		$s = '';
+    $s = '';
     // Possible characters
-		$letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
-		for( $i=0 ; $i<$len ; $i++ ){
+    for( $i=0 ; $i<$len ; $i++ ){
       // Grab a random letter for $letters
-			$char = $letters[mt_rand( 0 , strlen( $letters )-1 )];
-			$s .= $char; //Add it to the string
-		}
-		return $s;
-	}
+      $char = $letters[mt_rand( 0 , strlen( $letters )-1 )];
+      $s .= $char; //Add it to the string
+    }
+    return $s;
+  }
 
-	function hash( $password , $salt , $created_at ){
-		// Adds last two letters of the password for extra security
+  function hash( $password , $salt , $created_at ){
+    // Adds last two letters of the password for extra security
     $lastTwo = substr( $password , -2 );
     // Reverses the date and removes the dashes
-		$date = sha1( strrev( (string) $created_at ) );
+    $date = sha1( strrev( (string) $created_at ) );
     // Yay! Bcrypt
-		return crypt($salt . $lastTwo . $password . $date . $salt , '$2a$12$' . $salt);
-	}
+    return crypt($salt . $lastTwo . $password . $date . $salt , '$2a$12$' . $salt);
+  }
 
-	function salt(){
-		$firstSalt = substr( str_replace( '+' , '.' , base64_encode( sha1( microtime( true ) , true ) ) ) , 0 , 22 );
-		return $firstSalt;
-	}
-	
-	function register( $userName , $userPassword ){
-		if( $this->exists( $userName ) )
+  function salt(){
+    $firstSalt = substr( str_replace( '+' , '.' , base64_encode( sha1( microtime( true ) , true ) ) ) , 0 , 22 );
+    return $firstSalt;
+  }
+  
+  function register( $userName , $userPassword ){
+    if( $this->exists( $userName ) )
       return false;
     $salt = $this->salt(); //Generate a salt using the username provided
     $date = time();
@@ -52,10 +52,10 @@ class User {
       return true;
     die( mysql_error() ); // Run it. If it doesn't go through stop the script and display the error.
     return false;
-	}
+  }
   
   function update( $userName , $oldPassword , $newPassword ){
-		if( !$this->exists( $userName ) )
+    if( !$this->exists( $userName ) )
       return false;
     $q1 = sprintf( "SELECT password, rand, created_at FROM users WHERE username='%s'" ,
             mysql_real_escape_string( $userName )
@@ -77,78 +77,78 @@ class User {
       }
     }
   }
-	
-	function verify( $userName , $userPassword ){
-		// Grabbing all the user details with this query
+  
+  function verify( $userName , $userPassword ){
+    // Grabbing all the user details with this query
     $q1 = sprintf( "SELECT password, rand, created_at FROM users WHERE username='%s'" ,
             mysql_real_escape_string( $userName )
           );
-		$r1 = mysql_fetch_array( mysql_query( $q1 ) );
+    $r1 = mysql_fetch_array( mysql_query( $q1 ) );
     $ph = $this->hash( $userPassword , $r1['rand'] , $r1['created_at'] );
     // Return whether it is true or false
-		return ( $r1['password'] == $this->hash( $userPassword , $r1['rand'] , $r1['created_at'] ) );
-	}
+    return ( $r1['password'] == $this->hash( $userPassword , $r1['rand'] , $r1['created_at'] ) );
+  }
 
-	function setLoggedIn($userName, $userPassword) {
-		//This function is self explanitory :)
-		$_SESSION['loggedIn'] = true; 
-		$_SESSION['userName'] = $userName;
-		$_SESSION['userPassword'] = $userPassword;
-	}
-	
-	function isLoggedIn() { 
-		return ( isset( $_SESSION['loggedIn'] )
+  function setLoggedIn($userName, $userPassword) {
+    //This function is self explanitory :)
+    $_SESSION['loggedIn'] = true; 
+    $_SESSION['userName'] = $userName;
+    $_SESSION['userPassword'] = $userPassword;
+  }
+  
+  function isLoggedIn() { 
+    return ( isset( $_SESSION['loggedIn'] )
              && $_SESSION['loggedIn']
              && $this->verify( $_SESSION['userName'] , $_SESSION['userPassword'] ) );
-	}
-	
-	function redirectTo($page) { 
-		if( !headers_sent() ){
+  }
+  
+  function redirectTo($page) { 
+    if( !headers_sent() ){
       header( 'Location: ' . $page . '.php' );
     }
     die( '<a href="'.$page.'.php">Go to '.$page.'.php</a>' );
-	}
-	
-	function userInfo( $userName ){
-		// This function returns all user details to the front end. This is to save storing it all in sessions
+  }
+  
+  function userInfo( $userName ){
+    // This function returns all user details to the front end. This is to save storing it all in sessions
     $q1 = sprintf( "SELECT * FROM users WHERE username='%s'" ,
             mysql_real_escape_string( $userName )
           );
     // Fetch and Return the array
     return mysql_fetch_array( mysql_query( $q1 ) );
-	}
+  }
 
-	function userInfoId( $UID ){
-		// This function returns all user details to the front end. This is to save storing it all in sessions
+  function userInfoId( $UID ){
+    // This function returns all user details to the front end. This is to save storing it all in sessions
     $q1 = sprintf( "SELECT * FROM users WHERE id=%s" ,
             (int) $UID
           );
     // Fetch and Return the array
     return mysql_fetch_array( mysql_query( $q1 ) );
-	}
-	
-	function logOut(){
+  }
+  
+  function logOut(){
     // If they are logged in
-		if( isset( $_SESSION['loggedIn'] ) ){
+    if( isset( $_SESSION['loggedIn'] ) ){
       // Unset the session variables
-			unset( $_SESSION['loggedIn'] , $_SESSION['userName'] , $_SESSION['userPassword'] );
-			// Redirect to the login page
+      unset( $_SESSION['loggedIn'] , $_SESSION['userName'] , $_SESSION['userPassword'] );
+      // Redirect to the login page
       $this->redirectTo( 'login' );
-		}
-	}
-	
-	function exists( $userName ){
-		// Checks a user exists (for the register page)
+    }
+  }
+  
+  function exists( $userName ){
+    // Checks a user exists (for the register page)
     $q1 = sprintf( "SELECT username FROM users WHERE username = '%s'" ,
             mysql_real_escape_string( $userName )
           );
-		return (bool) mysql_num_rows( mysql_query( $q1 ) );
-	}
-	
-	function search( $field , $term ){
+    return (bool) mysql_num_rows( mysql_query( $q1 ) );
+  }
+  
+  function search( $field , $term ){
     $sql_field = false;
 
-		switch( $field ){
+    switch( $field ){
       case 'id' :
         $sql_field = 'id';
         break;
@@ -165,9 +165,9 @@ class User {
     if( !mysql_num_rows( $r1 ) )
       return false;
     return $r1;
-	}
-	
-	function messageNotification( $UID ){
+  }
+  
+  function messageNotification( $UID ){
     // Select all unread notifications
     $q1 = sprintf( "SELECT * FROM messages WHERE message_to = '%s' AND message_read = '0'" ,
             (int) $UID
@@ -175,12 +175,12 @@ class User {
     $r1 = mysql_query( $q1 );
     // Return the number
     return mysql_num_rows( $r1 );
-	}
-	
-	function displayMessages( $action , $UID , $ID=NULL ){
+  }
+  
+  function displayMessages( $action , $UID , $ID=NULL ){
     $where = false;
 
-		switch( $action ){
+    switch( $action ){
       case 'list' :
         $where = sprintf( "messages.message_to = %s ORDER BY messages.message_id DESC" ,
                    (int) $UID
@@ -201,23 +201,23 @@ class User {
       return false;
     return $r;
   }
-	
-	function setMessageStatus( $messageID , $status ){
+  
+  function setMessageStatus( $messageID , $status ){
     $q = sprintf( "UPDATE messages SET message_read = %s WHERE message_id = %s" ,
             (int) $status ,
             (int) $messageID
          );
-		mysql_query( $q );
-	}
+    mysql_query( $q );
+  }
 
-	function setMessageUnread( $messageID ){
-		setMessageStatus( $messageID , 0 );
-	}
-	function setMessageRead( $messageID ){
-		setMessageStatus( $messageID , 1 );
-	}
+  function setMessageUnread( $messageID ){
+    setMessageStatus( $messageID , 0 );
+  }
+  function setMessageRead( $messageID ){
+    setMessageStatus( $messageID , 1 );
+  }
 
-	function sendMessage( $to , $from , $subject , $message ){
+  function sendMessage( $to , $from , $subject , $message ){
     $q = sprintf( "INSERT INTO messages (message_to, message_from, message_subject, message, message_read) VALUES ('%s', '%s', '%s', '%s', 0)" ,
            mysql_real_escape_string( $to ) ,
            mysql_real_escape_string( $from ) ,
@@ -226,27 +226,27 @@ class User {
            
          );
     return mysql_query( $q ); //Inserts it. Notice the message read is set to 0
-	}
-	
-	function deleteMessage( $messageID ){
+  }
+  
+  function deleteMessage( $messageID ){
     $q = sprintf( "DELETE FROM messages WHERE message_id = '%s'" ,
            (int) $messageID
          );
-		return mysql_query( $q );
-	}
-	
-	function string_shorten( $text , $len ){
+    return mysql_query( $q );
+  }
+  
+  function string_shorten( $text , $len ){
     // Strip any linebreaks or multiple-spaces
     $text = preg_replace( array( "/\n|\r/" , '\s\s+' ) , ' ' , $text );
     // Split the text using the wordwrap() function
     $lines = explode( "\n" , wordwrap( $text , $len ) );
     // Get the First Line and add continuation ... sign
     return $lines[0].'...'; //Return the value
-	}
-	
-	function checkLevel( $i ){
+  }
+  
+  function checkLevel( $i ){
     $levels = array( 'Normal' , 'Moderator' , 'Admin' );
     return $levels[$i];
-	}
+  }
 
 }
